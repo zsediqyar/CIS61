a84cbe4af445a130109e30a5a41470263f903209
zaki <zaki.sediqyar@gmail.com>
2020-03-04 00:40:13 -0800

    lab5 files

---
+++ week_5/lab_5.py
@@ -0,0 +1,22 @@
+# QUESTION ONE
+def if_this_not_that(i_list, this):
+    for i in range(len(i_list)):
+        if i_list[i] > this:
+            print(i_list[i])
+        else:
+            print("that")
+
+
+sample = [1, 2, 3, 4, 5]
+if_this_not_that(sample, 3)
+
+# QUESTION TWO
+
+
+# QUESTION THREE
+# QUESTION FOUR
+# QUESTION FIVE
+# QUESTION SIX
+# QUESTION SEVEN
+# QUESTION EIGHT
+# QUESTION NINE
