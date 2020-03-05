a5469c5e2925bc217d770c1bd9baac56b4468f96
zaki <zaki.sediqyar@gmail.com>
2020-03-04 22:29:05 -0800

    lab 5 part one

--- week_5/lab_5.py
+++ week_5/lab_5.py
@@ -1,3 +1,6 @@
+from math import sqrt
+
+
 # QUESTION ONE
 def if_this_not_that(i_list, this):
     for i in range(len(i_list)):
@@ -10,13 +13,63 @@ def if_this_not_that(i_list, this):
 sample = [1, 2, 3, 4, 5]
 if_this_not_that(sample, 3)
 
+
 # QUESTION TWO
+def couple(list_one, list_two):
+    sample = []
+    for i in range(len(list_one)):
+        sample.append([list_one[i], list_two[i]])
+    return sample
+
+
+s = [1, 2, 3]
+b = [4, 5, 6]
+
+print(couple(s, b))
 
 
 # QUESTION THREE
+def enumerate(s, start=0):
+    new_list = []
+    for i in range(len(s)):
+        new_list.append([s[i], start])
+    return new_list
+
+
+print(enumerate('five', 5))
+
+
 # QUESTION FOUR
+def squares(n):
+    new_list = []
+    for i in range(len(n)):
+        if round(n[i] ** 0.5) ** 2 == n[i]:
+            new_list.append(int(sqrt(n[i])))
+    return new_list
+
+
+seq = [8, 49, 8, 9, 2, 1, 100, 102]
+
+print(squares(seq))
+
+
 # QUESTION FIVE
+
+
 # QUESTION SIX
+def flatten(lst):
+    flat_list = []
+    for items in lst:
+        if type(items) is list:
+            flat_list.extend(items)
+        else:
+            flat_list.append(items)
+    return flat_list
+
+
+x = [1, [2, 3], 4]
+print(flatten(x))
+
 # QUESTION SEVEN
 # QUESTION EIGHT
 # QUESTION NINE
